5b5244374478363fdf4efb191d6e84f87e20676d
xDDreki <dreki.o55@gmail.com>
2025-11-15 17:17:01 +0100

    XD

---
+++ main2.py
@@ -0,0 +1,112 @@
+"""
+- Dodac spoznienia/przyjscia wczesniej w umawianym
+- csv z danymi o id_Pacjenta, id_gabinetu, arrival_time, service_start_time, czas_oczekiwania, service_end_time, czas_
+- procesowanie danych
+*lekarze przerwa od pracy
+- naprawić żeby klienci nie wchodzili do gabinetu {service_time} przed zamknięciem
+"""
+import matplotlib
+
+matplotlib.use('TkAgg')  # or 'Qt5Agg'
+import numpy as np
+import simpy
+import seaborn
+import matplotlib.pyplot as plt
+import random
+
+
+class Pacjent:
+    def __init__(self, id):
+        self.id = id
+        self.arrival_time = None
+        self.service_start_time = None
+        self.service_end_time = None
+        self.room = None
+
+
+class Gabinet:
+    def __init__(self, id, env):
+        self.id = id
+        self.env = env
+        self.patients_served = 0
+        self.no_show = 0
+        self.resource = simpy.Resource(env, capacity=1)
+
+
+class Clinic:
+    def __init__(self, env, number_of_rooms, service_time, no_show=0.2, seed=None):
+        self.curr_patient_id = 1
+        self.env = env
+        self.service_time = service_time
+        self.no_show = no_show
+        self.seed = seed
+        self.list_rooms = [Gabinet(id=i + 1, env=self.env) for i in range(number_of_rooms)]
+        self.processed_patients = []
+        if self.seed:
+            random.seed(self.seed)
+
+    def czas(self):
+        hours = 9+self.env.now//60
+        minutes = self.env.now%60
+        if len(str(minutes))==1:
+            return f"{hours}:0{minutes}"
+        return f"{hours}:{minutes}"
+
+    def generate_patients(self, room):
+        def time_between_new_patients():
+            return self.service_time
+
+        while True:
+            patient = Pacjent(id=f"{room.id}.{self.curr_patient_id}")
+
+            if random.random() < self.no_show:
+                print(f'Czas {self.czas()}: Pacjent {patient.id} nie pojawił w gabinecie {room.id} w ciągu 15 minut')
+                patient.room = room.id
+                room.no_show += 1
+                self.curr_patient_id += 1
+                yield self.env.timeout(15)
+            else:
+                patient.arrival_time = self.env.now
+                print(f"Czas {self.czas()}: Pacjent {patient.id} przybył do kliniki")
+                patient.room = room.id
+                self.env.process(self.serve_patient(patient, room))
+                self.curr_patient_id += 1
+                yield self.env.timeout(time_between_new_patients())
+
+    def serve_patient(self, patient, room):
+        with room.resource.request() as request:
+            yield request
+            patient.service_start_time = self.env.now
+            print(f"Czas {self.czas()}: Pacjent {patient.id} wchodzi do gabinetu {room.id} ")
+            yield self.env.timeout(self.service_time)
+            patient.service_end_time = self.env.now
+            room.patients_served += 1
+            print(f"Czas {self.czas()}: Pacjent {patient.id} wychodzi z gabinetu {room.id}")
+            self.processed_patients.append(patient)
+
+    def run(self, sim_time):
+        for room in self.list_rooms:
+            self.env.process(self.generate_patients(room))
+        env.run(until=sim_time)
+
+    def stats(self):
+        def patient_bar_plot():
+            patients_served_ls = []
+            id_ls = []
+            for room in self.list_rooms:
+                patients_served_ls.append(room.patients_served)
+                id_ls.append(str(room.id))
+            seaborn.barplot(x=id_ls, y=patients_served_ls)
+            plt.savefig("patients_served_noshow.png")
+            plt.close()
+
+        patient_bar_plot()
+
+
+env = simpy.Environment()
+
+clinic = Clinic(env, number_of_rooms=3, service_time=20)
+
+clinic.run(420)
+
+# clinic.stats()
--- mainm.py
+++ mainm.py
@@ -1,4 +1,9 @@
+import matplotlib
+matplotlib.use('TkAgg')  # or 'Qt5Agg'
+import numpy as np
 import simpy
+import seaborn
+import matplotlib.pyplot as plt
 
 
 class Pacjent:
@@ -8,49 +13,48 @@ class Pacjent:
         self.service_start_time = None
         self.service_end_time = None
 
-
 class Gabinet:
     def __init__(self, id, env):
         self.id = id
         self.env = env
         self.patients_served = 0
 
-
 class Clinic:
-    def __init__(self, env, number_of_rooms, service_time):
+    def __init__(self, env, number_of_rooms, service_time, lambda_per_hour=14, seed=None):
         self.curr_patient_id = 1
         self.env = env
         self.service_time = service_time
-
-        temp_rooms = [Gabinet(id=i + 1, env=self.env) for i in range(number_of_rooms)]
+        self.lambda_per_hour = lambda_per_hour
+        self.seed = seed
+        self.list_rooms = [Gabinet(id=i + 1, env=self.env) for i in range(number_of_rooms)]
         self.rooms = simpy.Store(self.env, capacity=3)
-        for room in temp_rooms:
+        for room in self.list_rooms:
             self.rooms.put(room)
-        self.processed_patients =[]
+        self.processed_patients = []
 
     def generate_patients(self):
-
         def time_between_new_patients():
-            #uzupelnic poissona
-            return 10
+            if self.seed:
+                np.random.seed(self.seed)
+            return np.random.poisson(self.lambda_per_hour)
 
         while True:
             patient = Pacjent(id=self.curr_patient_id)
             patient.arrival_time = self.env.now
-            print(f"Czas {self.env.now}: Patient {patient.id} arrives")
+            print(f"Czas {self.env.now}: Pacjent {patient.id} przybył do kliniki")
             self.env.process(self.serve_patient(patient))
             self.curr_patient_id += 1
             yield self.env.timeout(time_between_new_patients())
 
     def serve_patient(self, patient):
         room = yield self.rooms.get()
-        print(f"Czas {self.env.now}: Patient {patient.id} enters Room {room.id} ")
+        print(f"Czas {self.env.now}: Pacjent {patient.id} wchodzi do gabinetu {room.id} ")
 
         yield self.env.timeout(self.service_time)
 
         patient.service_end_time = self.env.now
         room.patients_served += 1
-        print(f"Czas {self.env.now}: Patient {patient.id} leaves Room {room.id}")
+        print(f"Czas {self.env.now}: Pacjent {patient.id} wychodzi z gabinetu {room.id}")
         self.processed_patients.append(patient)
         yield self.rooms.put(room)
 
@@ -59,14 +63,22 @@ class Clinic:
         env.run(until=sim_time)
 
     def stats(self):
-        #uzupelnic
-        pass
+        def patient_bar_plot():
+            patients_served_ls = []
+            id_ls = []
+            for room in self.list_rooms:
+                patients_served_ls.append(room.patients_served)
+                id_ls.append(str(room.id))
+            seaborn.barplot(x=id_ls, y=patients_served_ls)
+            plt.savefig("patients_served.png")
+            plt.close()
+        patient_bar_plot()
 
 
 env = simpy.Environment()
 
 clinic = Clinic(env, number_of_rooms=3, service_time=15)
 
-clinic.run(420) 
+clinic.run(420)
 
-
+clinic.stats()
