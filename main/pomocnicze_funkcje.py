ecf74e825518eed1b8db3f74796dee1a4870bba5
xDDreki <dreki.o55@gmail.com>
2026-01-03 23:14:12 +0100

    Zastosowanie pomoczniczych funkcji i csv
    Dodanie eksportu do csv
    df używa teraz df.set_index(["day", "id"]) jako kluczy.
    Zaimplementowałem rozkład normalny dla czasu obsługi

--- main/pomocnicze_funkcje.py
+++ main/pomocnicze_funkcje.py
@@ -1,6 +1,11 @@
-def czas(self):
-    hours = 8 + self.env.now // 60
-    minutes = self.env.now % 60
+import numpy as np
+
+def readable_time(time):
+    hours = 8 + int(time // 60)
+    minutes = int(time % 60)
     if len(str(minutes)) == 1:
         return f"{hours}:0{minutes}"
     return f"{hours}:{minutes}"
+
+def randomized_service_time(mean=15, std=2, minimal_time=5):
+    return max(np.random.normal(mean, std), minimal_time)
